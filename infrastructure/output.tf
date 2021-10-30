08ea4a3ccf9df764522ea0363538f91df33bf5b7
Demola Adebayo <ademoladebayo@gmail.com>
2021-10-30 08:21:43 +0100

    output the necessary configuration

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -14,6 +14,6 @@ output "webserver_instance_ip" {
   value = aws_instance.webserver.public_ip
 }
 
-output "name_server" {
+/* output "name_server" {
   value = aws_route53_zone.this.name_servers
-}
+} */
